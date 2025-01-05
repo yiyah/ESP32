da2dd97c52b43081a94344f48d22dccc29efdfd4
yiyah <756307810@qq.com>
2025-01-05 17:32:59 +0800

    refactor(LED): :art: Update file structure

--- components/led/led.c
+++ components/led/led.c
@@ -1,15 +1,14 @@
-#include <stdio.h>
 #include "led.h"
 
 
 void LED_Init(void)
 {
     gpio_config_t io_conf = {0};
-    io_conf.intr_type = GPIO_INTR_DISABLE;
-    io_conf.mode = GPIO_MODE_INPUT_OUTPUT;
-    io_conf.pin_bit_mask = (1ULL << LED_GPIO_PIN);
-    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
-    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
 
+    io_conf.pin_bit_mask = (1ULL << LED_GPIO_PIN);
+    io_conf.mode         = GPIO_MODE_INPUT_OUTPUT;
+    io_conf.pull_down_en = GPIO_PULLDOWN_DISABLE;
+    io_conf.pull_up_en   = GPIO_PULLUP_ENABLE;
+    io_conf.intr_type    = GPIO_INTR_DISABLE;
     gpio_config(&io_conf);
 }
