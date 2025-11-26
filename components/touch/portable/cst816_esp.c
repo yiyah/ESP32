91c4d3407727e3c972af4436e955c2a460b4bd6c
yiyah <756307810@qq.com>
2025-11-26 22:54:03 +0800

    fix(touch): :bug: The pins of sda and scl are mixed up during initialization

--- components/touch/portable/cst816_esp.c
+++ components/touch/portable/cst816_esp.c
@@ -94,8 +94,8 @@ void port_iic_init()
 {
     i2c_config_t conf = {
         .mode = I2C_MODE_MASTER,
-        .sda_io_num = I2C_MASTER_SCL_IO,
-        .scl_io_num = I2C_MASTER_SDA_IO,
+        .sda_io_num = I2C_MASTER_SDA_IO,
+        .scl_io_num = I2C_MASTER_SCL_IO,
         .sda_pullup_en = GPIO_PULLUP_ENABLE,
         .scl_pullup_en = GPIO_PULLUP_ENABLE,
         .master.clk_speed = I2C_MASTER_FREQ_HZ,
